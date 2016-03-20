2ce4b1e2ed8346568f8338428fc64a2ac6468007
Bill Belanger <wj.belanger@gmail.com>
2016-03-19 22:27:24 -0400

    naming conventions
    Updated the resume.js file to use camel case instead of pascal case.
    Used ":hover" on the CSS instead of using javascript to toggle a class.

--- oldVersion/assets/scripts/resume.js
+++ oldVersion/assets/scripts/resume.js
@@ -1,35 +1,29 @@
-var StageSelectJumpTarget;
+var stageSelectJumpTarget;
 $(document).ready(function () {
-    $('div.stage').on('mouseenter', function () {
-        $('.selectbox', this).addClass('selected');
-    });
-    $('div.stage').on('mouseleave', function () {
-        $('.selectbox', this).removeClass('selected');
-    });
-    $('div.stage').on('click', StageSelected);
-	window.StageSelectJumpTarget = {};
-	window.StageSelectJumpTarget.left = $('#stage5 .selectbox img').offset().left - 20;
-	window.StageSelectJumpTarget.top = $('#stage3 .selectbox img').offset().top + 20;
+    $('div.stage').on('click', stageSelected);
+	window.stageSelectJumpTarget = {};
+	window.stageSelectJumpTarget.left = $('#stage5 .selectbox img').offset().left - 20;
+	window.stageSelectJumpTarget.top = $('#stage3 .selectbox img').offset().top + 20;
 });
 
-function StageSelected() {
+function stageSelected() {
 	var img = $('.selectbox img', this);
 	var text = $('p.descriptivetext', this);
 	if(img == null || img == undefined || img.length == 0) {
 		return; //TODO This is an error - handle it better
 	}
-	var copy = MakeCopy(img);
-	StageFlash(true); // Start the flashing
+	var copy = makeCopy(img);
+	stageFlash(true); // Start the flashing
 	$('.selectbox', this).addClass('completed'); // Mark the element as being seen
-	Jump(copy, function() {
-		StageFlash(false);
-		WriteText(text, function() {
+	jump(copy, function() {
+		stageFlash(false);
+		writeText(text, function() {
 			copy.remove();
 		});
 	});
 }
 
-function Jump(element, callback) {
+function jump(element, callback) {
 	var start = element.offset();
 	var bezier_params = {
         start: {
@@ -37,14 +31,14 @@ function Jump(element, callback) {
             y: start.top
         },
         end: {
-            x: window.StageSelectJumpTarget.left,
-            y: window.StageSelectJumpTarget.top
+            x: window.stageSelectJumpTarget.left,
+            y: window.stageSelectJumpTarget.top
         }
     };
 	element.animate({path : new $.path.bezier(bezier_params)}, 1000, 'linear', callback);
 }
 
-function WriteText(text, callback) {
+function writeText(text, callback) {
 	// Update the text
 	var p = text.clone();
 	p.css('margin', '0').css('display', 'none');
@@ -52,10 +46,10 @@ function WriteText(text, callback) {
 	p.fadeIn(1000); // TODO: Typewriter effect
 	
 	// Set up next click to return to previous page
-	$('#flashscreen').one('click', function() { ResetSelectScreen(callback); });
+	$('#flashscreen').one('click', function() { resetSelectScreen(callback); });
 }
 
-function ResetSelectScreen(callback) {
+function resetSelectScreen(callback) {
 	// Return to original screen
 	$('div.lightblue .text').html(''); // Clear the text for next time
 	$('#flashscreen').hide();
@@ -63,7 +57,7 @@ function ResetSelectScreen(callback) {
 	if(callback && typeof callback == 'function')
 		callback();
 }
-function MakeCopy(ele) {
+function makeCopy(ele) {
 	var pos = ele.offset();
 	var copy = ele.clone();
 	// Update the element to have an absolute top/left coordinates
@@ -76,39 +70,39 @@ function MakeCopy(ele) {
 	return copy;
 }
 
-var StageFlashTimer;
-var FlashWhiteScreen;
-var FlashTimeDuration = 150;
+var stageFlashTimer;
+var flashWhiteScreen;
+var flashTimeDuration = 150;
 // Experimental values based on trial & error
-var TimeInWhite = FlashTimeDuration / 7;
-var TimeInLines = FlashTimeDuration * (2 / 3);
-function StageFlash(toggle) {
+var timeInWhite = flashTimeDuration / 7;
+var timeInLines = flashTimeDuration * (2 / 3);
+function stageFlash(toggle) {
 	if(toggle) {
 		$('#main').hide();
 		$('#flashscreen').show();
-		window.FlashWhiteScreen = true; // Start out with the lines
-		ToggleFlashView();
-		window.StageFlashTimer = window.setInterval(ToggleFlashView, FlashTimeDuration);
+		window.flashWhiteScreen = true; // Start out with the lines
+		toggleFlashView();
+		window.stageFlashTimer = window.setInterval(toggleFlashView, flashTimeDuration);
 	}
 	else {
-		if(window.FlashWhiteScreen === true) { // Make sure we end on the lines
-			ToggleFlashView();
+		if(window.flashWhiteScreen === true) { // Make sure we end on the lines
+			toggleFlashView();
 		}
-		window.clearInterval(window.StageFlashTimer);
+		window.clearInterval(window.stageFlashTimer);
 	}
 }
-function ToggleFlashView() {
-	if(window.FlashWhiteScreen) {
+function toggleFlashView() {
+	if(window.flashWhiteScreen) {
 		// Set to line view
-		$('#flashscreen div.white').fadeOut(TimeInWhite, function() {
-			$('#flashscreen div.lines').fadeIn(TimeInLines);
+		$('#flashscreen div.white').fadeOut(timeInWhite, function() {
+			$('#flashscreen div.lines').fadeIn(timeInLines);
 		});
 	}
 	else {
 		// Set to white view
-		$('#flashscreen div.lines').fadeOut(TimeInLines, function() {
-			$('#flashscreen div.white').fadeIn(TimeInWhite);
+		$('#flashscreen div.lines').fadeOut(timeInLines, function() {
+			$('#flashscreen div.white').fadeIn(timeInWhite);
 		});
 	}
-	window.FlashWhiteScreen = ! window.FlashWhiteScreen;
+	window.flashWhiteScreen = ! window.flashWhiteScreen;
 }
