f045b2f1043004f085dfe2f111286e50551b5426
Bill Belanger <wj.belanger@gmail.com>
2016-03-20 21:56:58 -0400

    JSLint'd
    Updated according to JSLint specs

--- oldVersion/assets/scripts/resume.js
+++ oldVersion/assets/scripts/resume.js
@@ -1,27 +1,12 @@
+"use strict";
+/*global $*/
 var stageSelectJumpTarget;
-$(document).ready(function () {
-    $('div.stage').on('click', stageSelected);
-	window.stageSelectJumpTarget = {};
-	window.stageSelectJumpTarget.left = $('#stage5 .selectbox img').offset().left - 20;
-	window.stageSelectJumpTarget.top = $('#stage3 .selectbox img').offset().top + 20;
-});
-
-function stageSelected() {
-	var img = $('.selectbox img', this);
-	var text = $('p.descriptivetext', this);
-	if(img == null || img == undefined || img.length == 0) {
-		return; //TODO This is an error - handle it better
-	}
-	var copy = makeCopy(img);
-	stageFlash(true); // Start the flashing
-	$('.selectbox', this).addClass('completed'); // Mark the element as being seen
-	jump(copy, function() {
-		stageFlash(false);
-		writeText(text, function() {
-			copy.remove();
-		});
-	});
-}
+var stageFlashTimer;
+var flashWhiteScreen;
+var flashTimeDuration = 150;
+// Experimental values based on trial & error
+var timeInWhite = flashTimeDuration / 7;
+var timeInLines = flashTimeDuration * (2 / 3);
 
 function jump(element, callback) {
 	var start = element.offset();
@@ -38,6 +23,16 @@ function jump(element, callback) {
 	element.animate({path : new $.path.bezier(bezier_params)}, 1000, 'linear', callback);
 }
 
+function resetSelectScreen(callback) {
+	// Return to original screen
+	$('div.lightblue .text').html(''); // Clear the text for next time
+	$('#flashscreen').hide();
+	$('#main').show();
+	if (callback && typeof callback === 'function') {
+		callback();
+    }
+}
+
 function writeText(text, callback) {
 	// Update the text
 	var p = text.clone();
@@ -46,17 +41,9 @@ function writeText(text, callback) {
 	p.fadeIn(1000); // TODO: Typewriter effect
 	
 	// Set up next click to return to previous page
-	$('#flashscreen').one('click', function() { resetSelectScreen(callback); });
+	$('#flashscreen').one('click', function () { resetSelectScreen(callback); });
 }
 
-function resetSelectScreen(callback) {
-	// Return to original screen
-	$('div.lightblue .text').html(''); // Clear the text for next time
-	$('#flashscreen').hide();
-	$('#main').show();
-	if(callback && typeof callback == 'function')
-		callback();
-}
 function makeCopy(ele) {
 	var pos = ele.offset();
 	var copy = ele.clone();
@@ -70,39 +57,56 @@ function makeCopy(ele) {
 	return copy;
 }
 
-var stageFlashTimer;
-var flashWhiteScreen;
-var flashTimeDuration = 150;
-// Experimental values based on trial & error
-var timeInWhite = flashTimeDuration / 7;
-var timeInLines = flashTimeDuration * (2 / 3);
+function toggleFlashView() {
+	if (window.flashWhiteScreen) {
+		// Set to line view
+		$('#flashscreen div.white').fadeOut(timeInWhite, function () {
+			$('#flashscreen div.lines').fadeIn(timeInLines);
+		});
+	} else {
+		// Set to white view
+		$('#flashscreen div.lines').fadeOut(timeInLines, function () {
+			$('#flashscreen div.white').fadeIn(timeInWhite);
+		});
+	}
+	window.flashWhiteScreen = !window.flashWhiteScreen;
+}
+
 function stageFlash(toggle) {
-	if(toggle) {
+	if (toggle) {
 		$('#main').hide();
 		$('#flashscreen').show();
 		window.flashWhiteScreen = true; // Start out with the lines
 		toggleFlashView();
 		window.stageFlashTimer = window.setInterval(toggleFlashView, flashTimeDuration);
-	}
-	else {
-		if(window.flashWhiteScreen === true) { // Make sure we end on the lines
+	} else {
+		if (window.flashWhiteScreen === true) { // Make sure we end on the lines
 			toggleFlashView();
 		}
 		window.clearInterval(window.stageFlashTimer);
 	}
 }
-function toggleFlashView() {
-	if(window.flashWhiteScreen) {
-		// Set to line view
-		$('#flashscreen div.white').fadeOut(timeInWhite, function() {
-			$('#flashscreen div.lines').fadeIn(timeInLines);
-		});
+
+function stageSelected() {
+	var img = $('.selectbox img', this);
+	var text = $('p.descriptivetext', this);
+	if (img === null || img === undefined || img.length === 0) {
+		return; //TODO This is an error - handle it better
 	}
-	else {
-		// Set to white view
-		$('#flashscreen div.lines').fadeOut(timeInLines, function() {
-			$('#flashscreen div.white').fadeIn(timeInWhite);
+	var copy = makeCopy(img);
+	stageFlash(true); // Start the flashing
+	$('.selectbox', this).addClass('completed'); // Mark the element as being seen
+	jump(copy, function () {
+		stageFlash(false);
+		writeText(text, function () {
+			copy.remove();
 		});
-	}
-	window.flashWhiteScreen = ! window.flashWhiteScreen;
+	});
 }
+
+$(document).ready(function () {
+    $('div.stage').on('click', stageSelected);
+	window.stageSelectJumpTarget = {};
+	window.stageSelectJumpTarget.left = $('#stage5 .selectbox img').offset().left - 20;
+	window.stageSelectJumpTarget.top = $('#stage3 .selectbox img').offset().top + 20;
+});
